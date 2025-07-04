675e9da97fc0dfedd5a03d40c81dd748f38999f0
Oscar Eduardo Hincapié Vargas <oscar.hv89@gmail.com>
2025-07-04 15:12:28 -0500

    Using variables and creating sg

--- main.tf
+++ main.tf
@@ -14,11 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0]
+
+  egress_rules       = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0]
+
 }
